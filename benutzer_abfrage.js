00d150bfac9ff88d5442d36b49d7e8fe33e07308
Thomas Helfrich <info@helfrich-media.de>
2017-06-18 14:46:11 +0200

    Ausgabe des Benutzer Namen wenn Variable übereinstimmt.

---
+++ benutzer_abfrage.js
@@ -0,0 +1,19 @@
+/*
+ * Dies ist eine JavaScript-Umgebung.
+ *
+ * Geben Sie etwas JavaScript ein und führen Sie einen Rechtsklick aus oder wählen Sie aus dem Ausführen-Menü:
+ * 1. Ausführen, um den ausgewählten Text zu evaluieren (Strg+R),
+ * 2. Untersuchen, um den Objekt-Inspektor auf das Resultat anzuwenden (Strg+I), oder
+ * 3. Anzeigen, um das Ergebnis in einem Kommentar hinter der Auswahl einzufügen. (Strg+L)
+ */
+
+"use strict";
+
+var user = "Benutzer1";
+
+if(user == "Benutzer1"){
+  alert ("Herzlich Willkommen " + user + "!");
+}
+else {
+   alert("Der Name ist mir leider nicht bekannt.");
+}
