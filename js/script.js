7fcfac61226fb9727be73a89e89c0f08f9ca0940
Thomas Helfrich <info@helfrich-media.de>
2017-07-05 14:15:20 +0200

    HTML Struktur
    HTML Grundstruktur mit eingebundenem JavaScript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let user = prompt("Bitte gib deinen Namen ein");
+
+if(user == "Benutzer1"){
+    alert (`Herzlich Willkommen ${user}!`);
+}
+if(user == "Benutzer2"){
+  alert (`Herzlich Willkommen ${user}!`);
+}
+else {
+   alert("Der Name ist mir leider nicht bekannt.");
+}
