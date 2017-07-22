c8a9d225798afdf59aa6957d8d266785c7a00e42
Thomas Helfrich <info@helfrich-media.de>
2017-07-22 10:22:39 +0200

    JS Benutzer Abfrage deaktiviert

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-"use strict";
-
-let user = prompt("Bitte gib deinen Namen ein");
-
-if(user == "Benutzer1"){
-    alert (`Herzlich Willkommen ${user}!`);
-}
-if(user == "Benutzer2"){
-  alert (`Herzlich Willkommen ${user}!`);
-}
-else {
-   alert("Der Name ist mir leider nicht bekannt.");
-}
+// "use strict";
+//
+// let user = prompt("Bitte gib deinen Namen ein");
+//
+// if(user == "Benutzer1"){
+//     alert (`Herzlich Willkommen ${user}!`);
+// }
+// if(user == "Benutzer2"){
+//   alert (`Herzlich Willkommen ${user}!`);
+// }
+// else {
+//    alert("Der Name ist mir leider nicht bekannt.");
+// }
